ee95b4e91bcb81cbd45597bcca08e23a99176d27
sum-is-light <95518966+sum-is-light@users.noreply.github.com>
2021-12-04 14:58:51 +0800

    简单的猜数字游戏

--- hello.c
+++ hello.c
@@ -1,1 +1,17 @@
-
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+int main(){
+  srand(time(0));
+  int ai=rand()%50+1,play;
+  scanf("%d",&play);
+  while(play!=ai){
+    if(play>ai){
+      printf("你输入的数字太大了！\n");
+    }else{
+      printf("你输入的数过大！！！\n");
+    }
+    scanf("%d",&play);
+  }
+  return 0;
+}
